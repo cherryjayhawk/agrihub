22869e0b64d9ca03dd47c765ae7779ac65f57480
cherryjayhawk <muhbintangr19@gmail.com>
2025-09-13 18:58:21 +0700

    setup authjs

---
+++ auth.ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        console.log(credentials);
+        
+        const backendApiUrl = "https://backend-anda.com/api/login";
+
+        const res = await fetch(backendApiUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: credentials.email,
+            password: credentials.password,
+          }),
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+ 
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.user = token.user;
+      return session;
+    },
+  },
+});
