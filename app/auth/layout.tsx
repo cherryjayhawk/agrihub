af3b89656c9537bc2a118f74ee81a560bc2ce68e
cherryjayhawk <muhbintangr19@gmail.com>
2025-09-16 07:47:01 +0700

    add auth layout

---
+++ app/auth/layout.tsx
@@ -0,0 +1,36 @@
+import { GalleryVerticalEnd } from "lucide-react";
+import Image from "next/image";
+
+function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="font-medium text-2xl">
+            AgriculHub©
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">{children}</div>
+        </div>
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="font-light text-sm">
+            © AgriculHub {new Date().getFullYear()}
+          </a>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <Image
+          priority={true}
+          width={720}
+          height={1024}
+          src={"/auth-banner.png"}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MainLayout;
